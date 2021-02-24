a79e3d3a5a32088fd42634c377e34a5f09ad6f64
Antoni Kepinski <xxczaki@pm.me>
2021-02-25 00:22:13 +0100

    add more config options

--- readme.md
+++ readme.md
@@ -1,19 +1,24 @@
 # Storage Async
 
-> Lightweight, asynchronous, self-repairing, atomic, key-value JSON storage for Node.js applications. Supports TTL.
+> Lightweight, asynchronous, key-value JSON storage for Node.js applications. Supports TTL, atomic updates, self-repairing and more.
 
 [![Build Status](https://github.com/xxczaki/storage-async/workflows/CI/badge.svg)](https://github.com/xxczaki/storage-async/actions?query=workflow%3ACI)
 [![Coverage Status](https://coveralls.io/repos/github/xxczaki/storage-async/badge.svg?branch=master)](https://coveralls.io/github/xxczaki/storage-async?branch=master)
 [![XO code style](https://img.shields.io/badge/code_style-XO-5ed9c7.svg)](https://github.com/xojs/xo)
 ![minified size](https://img.shields.io/bundlephobia/minzip/storage-async)
 
-Description.
+This module aims to provide relatively lightweight and reliable way of locally storing JSON data, such as cache, preferences or temporary settings.
 
 ---
 
 ## Highlights
 
-TODO
+* **Lightweight.** About 30kB (minified and gzipped). Only 3 dependencies.
+* **Reliable.** Uses atomic file updates and can self-repair from invalid manual changes.
+* **Temporary.** Supports TTL (time-to-live).
+* **Familiar API.** Uses the same API as the [Map](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map) object.
+* **Well tested.** To make sure it handles various use cases correctly.
+* **Written in TypeScript.**
 
 ## Install
 
@@ -23,11 +28,61 @@ $ npm install storage-async
 
 ## Usage
 
-TODO
+```ts
+import {createStore} from 'storage-async';
+
+(async () => {
+	const store = await createStore();
+
+	await store.set('name', 'John);
+	await store.set('age', 20);
+
+	await store.has('name'); //=> true
+
+	await store.delete('age');
+
+	await store.get('name'); //=> 'John'
+
+	await store.clear();
+})();
+```
 
 ## API
 
-TODO
+### `createStore(options)`
+
+Creates a new store, which features the following API:
+
+```ts
+interface Handlers {
+	set: (key: string, value: unknown) => Promise<void>;
+	get: (key: string) => Promise<unknown>;
+	has: (key: string) => Promise<boolean>;
+	delete: (key: string) => Promise<void>;
+	clear: () => Promise<void>;
+}
+```
+
+##### `options`
+
+Type: `Options`
+
+Storage options:
+
+```ts
+interface Options {
+	path?: string;	// defaults to './store.json
+	ttl?: number;	// defaults to 900000ms (15 minutes)
+}
+```
+
+## How it works?
+
+1) When the store file (`./store.json` by default) does not exist, it will be created automatically.
+2) If the file can't be read (possibly due to invalid format), the module will attempt to repair it (to avoid data loss).
+3) If the repairing process can't finish, the module will delete it and create a new one (possibly resulting in data loss).
+4) If the file is valid and contains the information about TTL and creation time, the module will check whether the TTL hasn't yet expired.
+5) The module will now be able to read/write data. Writing is done atomically, so that unexpected shutdowns won't corrupt the store file.
 
 ## License
 
--- src/index.ts
+++ src/index.ts
@@ -3,41 +3,52 @@ import {writeFile, readFile} from 'fs/promises';
 import write from 'write-json-file';
 import read from 'load-json-file';
 import repair from 'jsonrepair';
+import {JsonObject, JsonArray} from 'type-fest';
+
+type JsonValue = string | number | boolean | JsonObject | JsonArray | null;
 
 interface Options {
 	path?: string;
 	ttl?: number;
-	selfRepair?: boolean;
-	regenAfterFail?: boolean;
+	resetOnFailure?: boolean;
+}
+
+interface InternalStructure {
+	__timestamp: number;
+	__ttl: number;
 }
 
 interface Handlers {
-	set: (key: string, value: unknown) => Promise<void>;
-	get: (key: string) => Promise<unknown>;
+	set: (key: string, value: JsonValue) => Promise<void>;
+	get: (key: string) => Promise<JsonValue | undefined>;
+	has: (key: string) => Promise<boolean>;
 	delete: (key: string) => Promise<void>;
 	clear: () => Promise<void>;
 }
 
 const defaults = {
 	path: join(__dirname, 'store.json'),
-	ttl: 900_000 // 15 minutes
+	ttl: 900_000, // 15 minutes
+	resetOnFailure: true
 };
 
 /**
- * Creates a new storage.
+ * Creates a new store.
  *
  * @param {Options} options - Options object.
- * @return {Handlers} Storage handlers (set of functions).
+ * @return {Handlers} Store handlers (set of functions).
  *
  * @example
- * format(new Date(2014, 1, 11), '{yyyy}-{MM}-{dd}') //=> '2014-01-11'
+ * const store = await createStore();
+ *
+ * await store.set('name', 'John');
  */
-export const createStore = async ({path, ttl}: Options = defaults): Promise<Handlers> => {
+export const createStore = async ({path, ttl, resetOnFailure}: Options = defaults): Promise<Handlers> => {
 	// Check if file already exists (flag wx)
 	try {
 		await writeFile(path ?? defaults.path, `{"__timestamp": ${Date.now()}, "__ttl": ${ttl ?? defaults.ttl}}`, {flag: 'wx'});
 	} catch {
-		// Check if the file corrupted
+		// Check if the file is corrupted
 		try {
 			await read(path ?? defaults.path);
 		} catch {
@@ -47,13 +58,18 @@ export const createStore = async ({path, ttl}: Options = defaults): Promise<Hand
 			try {
 				await writeFile(path ?? defaults.path, repair(content));
 			} catch {
-				await writeFile(path ?? defaults.path, `{"__timestamp": ${Date.now()}, "__ttl": ${ttl ?? defaults.ttl}}`);
+				// If without success - reset it.
+				if (resetOnFailure) {
+					await writeFile(path ?? defaults.path, `{"__timestamp": ${Date.now()}, "__ttl": ${ttl ?? defaults.ttl}}`);
+				} else {
+					throw new SyntaxError(`The storage file (available at ${path ?? defaults.path}) is invalid or corrupted and could not be repaired.`);
+				}
 			}
 		} finally {
-			const data = await read(path ?? defaults.path);
+			const data: InternalStructure | undefined = await read(path ?? defaults.path);
 
-			// @ts-expect-error
-			if (Date.now() - data?.__timestamp > data?.__ttl) {
+			// If the file's ttl has expired - reset it
+			if (Date.now() - (data?.__timestamp ?? 0) > (data?.__ttl ?? 0)) {
 				await writeFile(path ?? defaults.path,
 					`{"__timestamp": ${Date.now()}, "__ttl": ${ttl ?? defaults.ttl}}`
 				);
@@ -62,18 +78,75 @@ export const createStore = async ({path, ttl}: Options = defaults): Promise<Hand
 	}
 
 	return {
+		/**
+		* Adds element to the store.
+		*
+		* @param {string} key - Key, used to access the element.
+		* @param {JsonValue} value - A valid JSON value.
+		* @return {void}
+		*
+		* @example
+		* const store = await createStore();
+		*
+		* await store.set('name', 'John');
+		*/
 		set: async (key, _value) => {
 			const data = await read(path ?? defaults.path);
 
 			// @ts-expect-error
 			await write(path ?? defaults.path, {...data, [key]: _value});
 		},
+		/**
+		* Tries to access a store element by its key.
+		*
+		* @param {string} key - Key, used to access the element.
+		* @return {JsonValue} - A valid JSON value (or undefined, if not found)
+		*
+		* @example
+		* const store = await createStore();
+		*
+		* await store.set('name', 'John');
+	   	*
+	   	* await store.get('name'); //=> 'John'
+		*/
 		get: async key => {
 			const data = await read(path ?? defaults.path);
 
 			// @ts-expect-error
 			return data[key] ?? undefined;
 		},
+		/**
+		* Checks whether the element exists in the store.
+		*
+		* @param {string} key - Key, used to access the element.
+		* @return {boolean}
+		*
+		* @example
+		* const store = await createStore();
+		*
+		* await store.set('name', 'John');
+	   	*
+	   	* await store.has('name'); //=> true
+		*/
+		has: async key => {
+			const data = await read(path ?? defaults.path);
+
+			// @ts-expect-error
+			return typeof data[key] !== 'undefined';
+		},
+		/**
+		* Removes the element from store.
+		*
+		* @param {string} key - Key, used to access the element.
+		* @return {void}
+		*
+		* @example
+		* const store = await createStore();
+		*
+		* await store.set('name', 'John');
+	   	*
+	   	* await store.delete('name');
+		*/
 		delete: async key => {
 			const data = await read(path ?? defaults.path);
 
@@ -82,6 +155,16 @@ export const createStore = async ({path, ttl}: Options = defaults): Promise<Hand
 
 			await write(path ?? defaults.path, rest);
 		},
+		/**
+		* Removes all elements from the store.
+		*
+		* @return {void}
+		*
+		* @example
+		* const store = await createStore();
+	   	*
+	   	* await store.clear();
+		*/
 		clear: async () => {
 			await write(path ?? defaults.path, {});
 		}
